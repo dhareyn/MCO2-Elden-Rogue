39562b957ace9081f4ca9b7e28854a9fa448b210
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-03-29 19:22:29 +0800

    Removed ' to remove gibberish

--- GodslayersSeal.java
+++ GodslayersSeal.java
@@ -1,5 +1,5 @@
 public class GodslayersSeal extends Weapon {
     public GodslayersSeal() {
-        super("Godslayer’s Seal", 5000, 12, 15, 0, 35, 50, 0, 40);
+        super("Godslayers Seal", 5000, 12, 15, 0, 35, 50, 0, 40);
     }
 }
