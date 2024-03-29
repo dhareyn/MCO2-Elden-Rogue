4d209e6b7eed5d3cabe517f12269ed2088d22977
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-03-29 19:23:25 +0800

    removed ' to remove gibberish

--- MalikethsBlackBlade.java
+++ MalikethsBlackBlade.java
@@ -1,5 +1,5 @@
 public class MalikethsBlackBlade extends Weapon {
     public MalikethsBlackBlade() {
-        super("Maliketh’s Black Blade", 24000, 24, 30, 0, 40, 25, 80, 60);
+        super("Malikeths Black Blade", 24000, 24, 30, 0, 40, 25, 80, 60);
     }
 }
