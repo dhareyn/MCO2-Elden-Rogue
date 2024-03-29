7ebb952a95976ecd07df11c68a8ff6f8f9806aef
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-03-29 19:22:50 +0800

    Removed ' to remove gibberish

--- HoslowsPetalWhip.java
+++ HoslowsPetalWhip.java
@@ -1,5 +1,5 @@
 public class HoslowsPetalWhip extends Weapon {
     public HoslowsPetalWhip() {
-        super("Hoslow’s Petal Whip", 10000, 35, 35, 0, 20, 90, 55, 20);
+        super("Hoslows Petal Whip", 10000, 35, 35, 0, 20, 90, 55, 20);
     }
 }
