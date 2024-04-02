28529d4f8ba5ff88943e53d1722eca5cde925b76
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-02 16:57:32 +0800

    Updated with imagepath

--- CarianRegalScepter.java
+++ CarianRegalScepter.java
@@ -1,5 +1,5 @@
 public class CarianRegalScepter extends Weapon {
     public CarianRegalScepter() {
-        super("Carian Regal Scepter", 16000, 18, 25, 0, 85, 50, 20, 75);
+        super("Carian Regal Scepter", 16000, 18, 25, 0, 85, 50, 20, 75, "weaponpics/carianregalscepter.png");
     }
 }
