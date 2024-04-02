9816defdb33d973191bd04a72230004b1cb3b7c3
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-02 17:45:08 +0800

    Updated with imagepath

--- GodslayersSeal.java
+++ GodslayersSeal.java
@@ -1,5 +1,5 @@
 public class GodslayersSeal extends Weapon {
     public GodslayersSeal() {
-        super("Godslayers Seal", 5000, 12, 15, 0, 35, 50, 0, 40);
+        super("Godslayer's Seal", 5000, 12, 15, 0, 35, 50, 0, 40, "weaponpics/godslayersseal.png");
     }
 }
