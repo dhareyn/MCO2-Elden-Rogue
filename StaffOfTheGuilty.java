66d674de3578c2a667a34269818c62a08a6641b7
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-02 18:09:10 +0800

    updated with imagepath

--- StaffOfTheGuilty.java
+++ StaffOfTheGuilty.java
@@ -1,5 +1,5 @@
 public class StaffOfTheGuilty extends Weapon {
     public StaffOfTheGuilty() {
-        super("Staff of the Guilty", 8000, 16, 15, 0, 65, 40, 15, 60);
+        super("Staff of the Guilty", 8000, 16, 15, 0, 65, 40, 15, 60, "weaponpics/staffoftheguilty.png");
     }
 }
