c1728f43ac24dd9b9ab0f8988ec1ac551c992e98
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-02 16:56:59 +0800

    Updated with imagepath

--- AstrologersStaff.java
+++ AstrologersStaff.java
@@ -1,5 +1,5 @@
 public class AstrologersStaff extends Weapon {
     public AstrologersStaff() {
-        super("Astrologers Staff", 2000, 12, 5, 0, 25, 20, 5, 15);
+        super("Astrologers Staff", 2000, 12, 5, 0, 25, 20, 5, 15, "weaponpics/astrologersstaff.png");
     }
 }
