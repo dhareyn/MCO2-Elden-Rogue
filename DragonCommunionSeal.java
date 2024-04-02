a1a58dccf88c4e04f0a4cbb8b15189bd55496f66
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-02 17:04:28 +0800

    Updated with imagepath

--- DragonCommunionSeal.java
+++ DragonCommunionSeal.java
@@ -1,5 +1,5 @@
 public class DragonCommunionSeal extends Weapon {
     public DragonCommunionSeal() {
-        super("Dragon Communion Seal", 15000, 18, 25, 0, 75, 60, 0, 80);
+        super("Dragon Communion Seal", 15000, 18, 25, 0, 75, 60, 0, 80, "weaponpics/dragoncommunionseal.png");
     }
 }
