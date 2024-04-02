e82c82a6220f9a7902fcb4ddae698eca0f30755e
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-02 17:49:24 +0800

    Updated with imagepath

--- InseparableSword.java
+++ InseparableSword.java
@@ -1,5 +1,5 @@
 public class InseparableSword extends Weapon {
     public InseparableSword() {
-        super("Inseparable Sword", 12000, 19, 25, 0, 60, 20, 70, 60);
+        super("Inseparable Sword", 12000, 19, 25, 0, 60, 20, 70, 60, "weaponpics/inseparablesword");
     }
 }
