ac24d7403135a554ba8eec4b78dcb2b1474179a5
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-02 17:48:15 +0800

    Updated with imagepath

--- HoslowsPetalWhip.java
+++ HoslowsPetalWhip.java
@@ -1,5 +1,5 @@
 public class HoslowsPetalWhip extends Weapon {
     public HoslowsPetalWhip() {
-        super("Hoslows Petal Whip", 10000, 35, 35, 0, 20, 90, 55, 20);
+        super("Hoslow's Petal Whip", 10000, 35, 35, 0, 20, 90, 55, 20, "weaponpics/hoslowspetalwhip.png");
     }
 }
