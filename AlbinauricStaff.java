a2f23107663f6a48665cd6050faf1ee697e2b4cb
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-02 16:55:42 +0800

    Updated with imagepath

--- AlbinauricStaff.java
+++ AlbinauricStaff.java
@@ -1,5 +1,5 @@
 public class AlbinauricStaff extends Weapon {
     public AlbinauricStaff() {
-        super("Albinauric Staff", 4000, 14, 10, 0, 45, 30, 10, 35);
+        super("Albinauric Staff", 4000, 14, 10, 0, 45, 30, 10, 35, "weaponpics/albinauricstaff.png");
     }
 }
