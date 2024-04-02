8f32b93e372ed90d17f9f34c1420a0a40af9acea
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-02 18:04:37 +0800

    Updated with imagepath

--- Samurai.java
+++ Samurai.java
@@ -1,6 +1,6 @@
 public class Samurai extends JobClass {
     public Samurai() {
-        super(9, 12, 15, 9, 13, 12, 8);
+        super(9, 12, 15, 9, 13, 12, 8, "jobpics/samurai.png");
     }
 
     @Override
