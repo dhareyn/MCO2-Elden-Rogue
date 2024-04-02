a48411624d8012ed9af7a13c6503045abcc505d1
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-02 17:04:47 +0800

    Updated with imagepath

--- CodedSword.java
+++ CodedSword.java
@@ -1,5 +1,5 @@
 public class CodedSword extends Weapon {
     public CodedSword() {
-        super("Coded Sword", 4000, 21, 20, 0, 40, 35, 40, 40, "weaponpics/codedsword");
+        super("Coded Sword", 4000, 21, 20, 0, 40, 35, 40, 40, "weaponpics/codedsword.png");
     }
 }
