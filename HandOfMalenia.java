20315037b65577b096d5cf3fabc4c22a5cd74300
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-02 17:46:24 +0800

    Updated with imagepath

--- HandOfMalenia.java
+++ HandOfMalenia.java
@@ -1,5 +1,5 @@
 public class HandOfMalenia extends Weapon {
     public HandOfMalenia() {
-        super("Hand of Malenia", 15000, 30, 50, 0, 0, 50, 75, 0);
+        super("Hand of Malenia", 15000, 30, 50, 0, 0, 50, 75, 0, "weaponpics/handofmalenia.png");
     }
 }
