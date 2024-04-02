36227d5e348b7c8ce6433f8dfab68af79ab5d748
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-02 18:09:45 +0800

    updated with imagepath

--- StarscourageGreatsword.java
+++ StarscourageGreatsword.java
@@ -1,5 +1,5 @@
 public class StarscourageGreatsword extends Weapon {
     public StarscourageGreatsword() {
-        super("Starscourage Greatsword", 6000, 14, 20, 0, 0, 15, 40, 20);
+        super("Starscourage Greatsword", 6000, 14, 20, 0, 0, 15, 40, 20, "weaponpics/starscouragegreatsword.png");
     }
 }
