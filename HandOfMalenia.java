56550e3c9c2a99462a6f5ce442f636a9a1263fc9
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:25:46 +0800

    Update HandOfMalenia.java

--- HandOfMalenia.java
+++ HandOfMalenia.java
@@ -1,4 +1,10 @@
+/**
+ * Represents Hand of Malenia weapon.
+ */
 public class HandOfMalenia extends Weapon {
+    /**
+     * Constructs a Hand of Malenia with preset attributes.
+     */
     public HandOfMalenia() {
         super("Hand of Malenia", 15000, 30, 50, 0, 0, 50, 75, 0, "weaponpics/handofmalenia.png");
     }
