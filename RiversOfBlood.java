5a69b1e6848ee5b795cb53e5c93c21e1295fdbaa
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:22:00 +0800

    Update RiversOfBlood.java

--- RiversOfBlood.java
+++ RiversOfBlood.java
@@ -1,4 +1,10 @@
+/**
+ * Represents the Rivers of Blood weapon, extending the Weapon class.
+ */
 public class RiversOfBlood extends Weapon {
+    /**
+     * Constructs a Rivers of Blood object with default attributes.
+     */
     public RiversOfBlood() {
         super("Rivers of Blood", 7500, 25, 40, 0, 0, 45, 60, 0, "weaponpics/riversofblood.png");
     }
