6cf6bbed427fb0e21617022b8b6804a089992284
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:23:18 +0800

    Update Moonveil.java

--- Moonveil.java
+++ Moonveil.java
@@ -1,4 +1,10 @@
+/**
+ * Represents the Moonveil weapon, extending the Weapon class.
+ */
 public class Moonveil extends Weapon {
+    /**
+     * Constructs a Moonveil object with default attributes.
+     */
     public Moonveil() {
         super("Moonveil", 3750, 20, 30, 0, 0, 40, 45, 0, "weaponpics/moonveil.png");
     }
