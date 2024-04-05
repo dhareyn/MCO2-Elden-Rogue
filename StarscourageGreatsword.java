e4b551ff9cb33a943a8444d02bae7ec9ed4c63fa
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:15:37 +0800

    Update StarscourageGreatsword.java

--- StarscourageGreatsword.java
+++ StarscourageGreatsword.java
@@ -1,4 +1,11 @@
+/**
+ * Represents the Starscourage Greatsword, a specific type of weapon.
+ */
 public class StarscourageGreatsword extends Weapon {
+    
+    /**
+     * Constructs a new Starscourage Greatsword object.
+     */
     public StarscourageGreatsword() {
         super("Starscourage Greatsword", 6000, 14, 20, 0, 0, 15, 40, 20, "weaponpics/starscouragegreatsword.png");
     }
