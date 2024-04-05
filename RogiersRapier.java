9b397a40eb53e1cfbd13be282c2a4f21af4bfee3
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:21:43 +0800

    Update RogiersRapier.java

--- RogiersRapier.java
+++ RogiersRapier.java
@@ -1,4 +1,10 @@
+/**
+ * Represents the Rogier's Rapier weapon, extending the Weapon class.
+ */
 public class RogiersRapier extends Weapon {
+    /**
+     * Constructs a Rogier's Rapier object with default attributes.
+     */
     public RogiersRapier() {
         super("Rogier's Rapier", 2000, 18, 10, 0, 35, 25, 35, 35, "weaponpics/rogiersrapier.png");
     }
