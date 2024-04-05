7c734644b76df54afb223a5b15da603cdb66513b
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:26:05 +0800

    Update GoldenOrderSeal.java

--- GoldenOrderSeal.java
+++ GoldenOrderSeal.java
@@ -1,4 +1,10 @@
+/**
+ * Represents Golden Order Seal weapon.
+ */
 public class GoldenOrderSeal extends Weapon {
+    /**
+     * Constructs a Golden Order Seal with preset attributes.
+     */
     public GoldenOrderSeal() {
         super("Golden Order Seal", 10000, 14, 20, 0, 65, 55, 0, 65, "weaponpics/goldenorderseal.png");
     }
