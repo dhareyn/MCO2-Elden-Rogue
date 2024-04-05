a120318176592ea588841cb79114b605a804c935
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:15:18 +0800

    Update SwordOfNightAndFlame.java

--- SwordOfNightAndFlame.java
+++ SwordOfNightAndFlame.java
@@ -1,4 +1,11 @@
+/**
+ * Represents the Sword of Night and Flame, a specific type of weapon.
+ */
 public class SwordOfNightAndFlame extends Weapon {
+    
+    /**
+     * Constructs a new Sword of Night and Flame object.
+     */
     public SwordOfNightAndFlame() {
         super("Sword of Night and Flame", 8000, 25, 30, 0, 55, 45, 55, 55, "weaponpics/swordofthenightandflame.png");
     }
