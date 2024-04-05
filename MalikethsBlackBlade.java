30aaf3dd8ec3475fbc067265d99f9885400dcacf
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:23:35 +0800

    Update MalikethsBlackBlade.java

--- MalikethsBlackBlade.java
+++ MalikethsBlackBlade.java
@@ -1,4 +1,10 @@
+/**
+ * Represents Maliketh's Black Blade, extending the Weapon class.
+ */
 public class MalikethsBlackBlade extends Weapon {
+    /**
+     * Constructs a Maliketh's Black Blade object with default attributes.
+     */
     public MalikethsBlackBlade() {
         super("Maliketh's Black Blade", 24000, 24, 30, 0, 40, 25, 80, 60, "weaponpics/malikethsblackblade.png");
     }
