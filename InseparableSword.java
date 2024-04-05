dc5000c3de6387c207965caa01cf0a6528ba2092
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:25:06 +0800

    Update InseparableSword.java

--- InseparableSword.java
+++ InseparableSword.java
@@ -1,4 +1,10 @@
+/**
+ * Represents the Inseparable Sword weapon.
+ */
 public class InseparableSword extends Weapon {
+    /**
+     * Constructs an Inseparable Sword with preset attributes.
+     */
     public InseparableSword() {
         super("Inseparable Sword", 12000, 19, 25, 0, 60, 20, 70, 60, "weaponpics/inseparablesword");
     }
