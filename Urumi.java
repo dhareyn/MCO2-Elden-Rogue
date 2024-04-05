ae73536c28f76f83e8cbbb577bf0b11df1a68479
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:12:12 +0800

    Update Urumi.java

--- Urumi.java
+++ Urumi.java
@@ -1,4 +1,11 @@
+/**
+ * Represents an Urumi, a specific type of weapon.
+ */
 public class Urumi extends Weapon {
+    
+    /**
+     * Constructs a new Urumi object.
+     */
     public Urumi() {
         super("Urumi", 5000, 30, 20, 0, 10, 70, 40, 0, "weaponpics/urumi.png");
     }
