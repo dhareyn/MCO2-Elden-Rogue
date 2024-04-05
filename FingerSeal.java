2fae3dda6476ab284a7242f80657ebe2ee2381c5
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:08:40 +0800

    Update FingerSeal.java

--- FingerSeal.java
+++ FingerSeal.java
@@ -1,4 +1,11 @@
+/**
+ * Represents a Finger Seal, a specific type of weapon.
+ */
 public class FingerSeal extends Weapon {
+    
+    /**
+     * Constructs a new Finger Seal object.
+     */
     public FingerSeal() {
         super("Finger Seal", 2500, 10, 10, 0, 15, 45, 0, 40, "weaponpics/fingerseal.png");
     }
