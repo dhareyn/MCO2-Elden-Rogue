3049b5c832b0085bf90bb37cc2a32aecc2388567
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 11:56:17 +0800

    Update CarianRegalScepter.java

--- CarianRegalScepter.java
+++ CarianRegalScepter.java
@@ -1,4 +1,11 @@
+/**
+ * Represents a Carian Regal Scepter, a specific type of weapon.
+ */
 public class CarianRegalScepter extends Weapon {
+
+    /**
+     * Constructs a new Carian Regal Scepter object.
+     */
     public CarianRegalScepter() {
         super("Carian Regal Scepter", 16000, 18, 25, 0, 85, 50, 20, 75, "weaponpics/carianregalscepter.png");
     }
