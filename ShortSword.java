a5c1695c1bbb5afe9219a6a6879edfc0e4140c0a
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:16:15 +0800

    Update ShortSword.java

--- ShortSword.java
+++ ShortSword.java
@@ -1,4 +1,11 @@
+/**
+ * Represents a Short Sword, a specific type of weapon.
+ */
 public class ShortSword extends Weapon {
+    
+    /**
+     * Constructs a new Short Sword object.
+     */
     public ShortSword() {
         super("Short Sword", 1000, 13, 0, 0, 15, 15, 15, 15, "weaponpics/shortsword.png");
     }
