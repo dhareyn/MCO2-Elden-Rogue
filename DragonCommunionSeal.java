c8d7df324bac4a8bace0b26cc9dbcc1e597c1aa0
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:08:17 +0800

    Update DragonCommunionSeal.java

--- DragonCommunionSeal.java
+++ DragonCommunionSeal.java
@@ -1,4 +1,11 @@
+/**
+ * Represents a Dragon Communion Seal, a specific type of weapon.
+ */
 public class DragonCommunionSeal extends Weapon {
+    
+    /**
+     * Constructs a new Dragon Communion Seal object.
+     */
     public DragonCommunionSeal() {
         super("Dragon Communion Seal", 15000, 18, 25, 0, 75, 60, 0, 80, "weaponpics/dragoncommunionseal.png");
     }
