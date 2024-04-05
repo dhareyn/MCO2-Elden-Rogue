0aaccfa4f4239eef91ddf76febd19f808bd57dd3
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:05:36 +0800

    Update CharacterCreationModel.java

--- CharacterCreationModel.java
+++ CharacterCreationModel.java
@@ -1,29 +1,60 @@
+/**
+ * Represents the model for character creation, managing player name and selected job class.
+ */
 public class CharacterCreationModel {
     private String playerName;
     private JobClass selectedJobClass;
 
+    /**
+     * Constructs a new CharacterCreationModel with default settings.
+     */
     public CharacterCreationModel() {
-        // Initialize any default values or settings here
+        this.playerName = null;
+        this.selectedJobClass = null;
     }
 
-    // Getter and setter methods for playerName and selectedJobClass
-
+    /**
+     * Retrieves the player's name.
+     *
+     * @return the player's name
+     */
     public String getPlayerName() {
         return playerName;
     }
 
+    /**
+     * Sets the player's name.
+     *
+     * @param playerName the player's name to set
+     */
     public void setPlayerName(String playerName) {
         this.playerName = playerName;
     }
 
+    /**
+     * Retrieves the selected job class.
+     *
+     * @return the selected job class
+     */
     public JobClass getSelectedJobClass() {
         return selectedJobClass;
     }
 
+    /**
+     * Sets the selected job class.
+     *
+     * @param selectedJobClass the selected job class to set
+     */
     public void setSelectedJobClass(JobClass selectedJobClass) {
         this.selectedJobClass = selectedJobClass;
     }
 
+    /**
+     * Retrieves the JobClass object based on the selected class name.
+     *
+     * @param selectedClass the name of the selected class
+     * @return the corresponding JobClass object
+     */
     public JobClass getJobClass(String selectedClass) {
         switch (selectedClass) {
             case "Vagabond":
