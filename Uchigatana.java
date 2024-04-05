95b43731e35d52530991ab281008e7ee1401db4f
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:12:29 +0800

    Update Uchigatana.java

--- Uchigatana.java
+++ Uchigatana.java
@@ -1,4 +1,11 @@
+/**
+ * Represents an Uchigatana, a specific type of weapon.
+ */
 public class Uchigatana extends Weapon {
+    
+    /**
+     * Constructs a new Uchigatana object.
+     */
     public Uchigatana() {
         super("Uchigatana", 1875, 15, 20, 0, 0, 35, 30, 0, "weaponpics/uchigatana.png");
     }
