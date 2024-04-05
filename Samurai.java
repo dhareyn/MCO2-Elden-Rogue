9c8a36d7891b808c2d5545e49da1828990a390fa
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:21:09 +0800

    Update Samurai.java

--- Samurai.java
+++ Samurai.java
@@ -1,8 +1,18 @@
+/**
+ * Represents the Samurai job class, extending the JobClass abstract class.
+ */
 public class Samurai extends JobClass {
+    /**
+     * Constructs a Samurai object with default attributes.
+     */
     public Samurai() {
         super(9, 12, 15, 9, 13, 12, 8, "jobpics/samurai.png");
     }
 
+    /**
+     * Retrieves the name of the job.
+     * @return The name of the job ("Samurai").
+     */
     @Override
     public String getJobName() {
         return "Samurai";
