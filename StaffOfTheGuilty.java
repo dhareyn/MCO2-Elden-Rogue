2c0c425de9c2d4ca1f15152d56ebec525faca0e4
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:15:55 +0800

    Update StaffOfTheGuilty.java

--- StaffOfTheGuilty.java
+++ StaffOfTheGuilty.java
@@ -1,4 +1,11 @@
+/**
+ * Represents the Staff of the Guilty, a specific type of weapon.
+ */
 public class StaffOfTheGuilty extends Weapon {
+    
+    /**
+     * Constructs a new Staff of the Guilty object.
+     */
     public StaffOfTheGuilty() {
         super("Staff of the Guilty", 8000, 16, 15, 0, 65, 40, 15, 60, "weaponpics/staffoftheguilty.png");
     }
