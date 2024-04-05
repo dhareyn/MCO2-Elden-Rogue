7694c4d3ec95deb744c19521f5356ef8ce6f2b5f
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:25:24 +0800

    Update HoslowsPetalWhip.java

--- HoslowsPetalWhip.java
+++ HoslowsPetalWhip.java
@@ -1,4 +1,10 @@
+/**
+ * Represents Hoslow's Petal Whip weapon.
+ */
 public class HoslowsPetalWhip extends Weapon {
+    /**
+     * Constructs a Hoslow's Petal Whip with preset attributes.
+     */
     public HoslowsPetalWhip() {
         super("Hoslow's Petal Whip", 10000, 35, 35, 0, 20, 90, 55, 20, "weaponpics/hoslowspetalwhip.png");
     }
