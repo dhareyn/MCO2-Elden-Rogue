3985adf333f555181adca367b3d2bc110b2400b4
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:12:49 +0800

    Update ThornedWhip.java

--- ThornedWhip.java
+++ ThornedWhip.java
@@ -1,4 +1,11 @@
+/**
+ * Represents a Thorned Whip, a specific type of weapon.
+ */
 public class ThornedWhip extends Weapon {
+    
+    /**
+     * Constructs a new Thorned Whip object.
+     */
     public ThornedWhip() {
         super("Thorned Whip", 5000, 30, 30, 0, 0, 80, 20, 40, "weaponpics/thornedwhip.png");
     }
