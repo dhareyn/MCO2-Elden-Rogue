92208f6eeb391aa92a5d160a46e4712822c8e7ff
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:26:22 +0800

    Update GodslayersSeal.java

--- GodslayersSeal.java
+++ GodslayersSeal.java
@@ -1,4 +1,10 @@
+/**
+ * Represents Godslayer's Seal weapon.
+ */
 public class GodslayersSeal extends Weapon {
+    /**
+     * Constructs a Godslayer's Seal with preset attributes.
+     */
     public GodslayersSeal() {
         super("Godslayer's Seal", 5000, 12, 15, 0, 35, 50, 0, 40, "weaponpics/godslayersseal.png");
     }
