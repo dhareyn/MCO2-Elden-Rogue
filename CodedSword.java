a7c5db3d2d60737f30ab996021f6544bd08b9efa
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:07:58 +0800

    Update CodedSword.java

--- CodedSword.java
+++ CodedSword.java
@@ -1,4 +1,11 @@
+/**
+ * Represents a Coded Sword, a specific type of weapon.
+ */
 public class CodedSword extends Weapon {
+    
+    /**
+     * Constructs a new Coded Sword object.
+     */
     public CodedSword() {
         super("Coded Sword", 4000, 21, 20, 0, 40, 35, 40, 40, "weaponpics/codedsword.png");
     }
