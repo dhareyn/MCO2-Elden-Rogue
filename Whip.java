f68a96fce691a31ff450e5eef4fb8118077098a4
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:10:56 +0800

    Update Whip.java

--- Whip.java
+++ Whip.java
@@ -1,4 +1,11 @@
+/**
+ * Represents a Whip, a specific type of weapon.
+ */
 public class Whip extends Weapon {
+    
+    /**
+     * Constructs a new Whip object.
+     */
     public Whip() {
         super("Whip", 1500, 20, 15, 0, 0, 60, 20, 0, "weaponpics/whip.png");
     }
