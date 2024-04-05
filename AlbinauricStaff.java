ecd72c78b83c559ad5c1623796a26d3096c6a05c
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 11:55:24 +0800

    update

--- AlbinauricStaff.java
+++ AlbinauricStaff.java
@@ -1,4 +1,11 @@
+/**
+ * Represents an Albinauric Staff, a specific type of weapon.
+ */
 public class AlbinauricStaff extends Weapon {
+
+    /**
+     * Constructs a new AlbinauricStaff object.
+     */
     public AlbinauricStaff() {
         super("Albinauric Staff", 4000, 14, 10, 0, 45, 30, 10, 35, "weaponpics/albinauricstaff.png");
     }
