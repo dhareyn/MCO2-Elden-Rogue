a3c1c710effa1d3cd5aaa9edee84c7709eab9a4b
Darren Sanchez <126580817+dhareyn@users.noreply.github.com>
2024-04-05 12:07:36 +0800

    Update Claymore.java

--- Claymore.java
+++ Claymore.java
@@ -1,4 +1,11 @@
+/**
+ * Represents a Claymore, a specific type of weapon.
+ */
 public class Claymore extends Weapon {
+    
+    /**
+     * Constructs a new Claymore object.
+     */
     public Claymore() {
         super("Claymore", 3000, 9, 15, 0, 0, 10, 20, 0, "weaponpics/claymore.png");
     }
